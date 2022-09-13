2b41f664f534789a5bec316a8d7d87998e0f6790
danchohr9 <87618737+danchohr9@users.noreply.github.com>
2022-09-13 11:05:36 +0300

    Adding test files

---
+++ Bus Transportation/BusTransportationTest/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("BusTransportationTest")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("BusTransportationTest")]
+[assembly: AssemblyCopyright("Copyright ©  2022")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("4d6d4bf6-c04f-48f3-ab86-83bf9c4cdedb")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ Bus Transportation/BusTransportationTest/UnitTest1.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace BusTransportationTest
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        //Bus Testing
+        [TestMethod]
+        public void Test_IfBusDoesntExist()
+        {
+            BusTransportation.BusForm form = new BusTransportation.BusForm();
+
+            string plate = "CC9696CA";
+            bool actual = form.checkIfBusDoesntExists(plate);
+            Assert.AreEqual(false, actual);
+
+            plate = "asdasdasdas";
+            actual = form.checkIfBusDoesntExists(plate);
+            Assert.AreEqual(true, actual);
+        }
+
+        //Edit Ticket Testing
+        [TestMethod]
+        public void Test_getIdByUsername()
+        {
+            BusTransportation.EditTicket form = new BusTransportation.EditTicket();
+
+            string user = "a";
+            string actual = form.getIdByUsername(user);
+            Assert.AreEqual("13", actual);
+        }
+
+        [TestMethod]
+        public void Test_getUser()
+        {
+            BusTransportation.EditTicket form = new BusTransportation.EditTicket();
+
+            string userId = "13";
+            string actual = form.getUser(userId);
+            Assert.AreEqual("13: a", actual);
+        }
+
+        [TestMethod]
+        public void Test_getTransport()
+        {
+            BusTransportation.EditTicket form = new BusTransportation.EditTicket();
+
+            string transportId = "1";
+            string actual = form.getTransport(transportId);
+            Assert.AreEqual("1: Благоевград->Кюстендил(5/1/2022 12:12:51 PM)", actual);
+        }
+
+        [TestMethod]
+        public void Test_searchBus()
+        {
+            BusTransportation.EditTicket form = new BusTransportation.EditTicket();
+
+            string transportId = "1";
+            int actual = form.searchBus(transportId);
+            Assert.AreEqual(1, actual); //maluk
+        }
+
+        [TestMethod]
+        public void Test_getTransportPrice()
+        {
+            BusTransportation.EditTicket form = new BusTransportation.EditTicket();
+
+            string transportId = "1";
+            string actual = form.getTransportPrice(transportId);
+            Assert.AreEqual("22", actual);
+        }
+
+        //Register testing
+
+        [TestMethod]
+        public void Test_isValidEmail()
+        {
+            BusTransportation.Form1 form = new BusTransportation.Form1();
+
+            string email = "testemail@abv.bg";
+            bool actual = form.isValidEmail(email);
+            Assert.AreEqual(true, actual);
+
+            email = "incorrectemail";
+            actual = form.isValidEmail(email);
+            Assert.AreEqual(false, actual);
+        }
+
+        [TestMethod]
+        public void Test_checkIfUserDoesntExist()
+        {
+            BusTransportation.Form1 form = new BusTransportation.Form1();
+
+            string user = "a";
+            bool actual = form.checkIfUserDoesntExist(user);
+            Assert.AreEqual(false, actual);   //it exists
+
+            user = "doesntexist";
+            actual = form.checkIfUserDoesntExist(user);
+            Assert.AreEqual(true, actual);
+        }
+
+
+
+    }
+}
